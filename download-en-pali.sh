236b7a805aff6942d55270000899c11ab1e1d485
Rajan Saini <rajansaini@cs.ucsb.edu>
2021-11-29 18:32:31 -0800

    Improve docs for pali download script

--- download-en-pali.sh
+++ download-en-pali.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-##########################################################################
-# This script generates data/en-pali/train.en and data/en-pali/train.pli #
-##########################################################################
+###########################################################################
+# This script generates data/en-pali/train.en and data/en-pali/train.pli. #
+# The other files in data/en-pali are intermediate and can be ignored. 	  #
+###########################################################################
 
-#mkdir -p data/
-#mkdir -p data/en-pali
-#cd data/en-pali
-#echo "Cloning raw data source"
-#git clone https://github.com/suttacentral/bilara-data
-#cd bilara-data/.scripts/bilara-io
-#./sheet_export.py vinaya vinaya.tsv --include root,translation+en
-#./sheet_export.py sutta sutta.tsv --include root,translation+en
-#cp vinaya.tsv ../../../
-#cp sutta.tsv ../../../
-#cd ../../../../..
+mkdir -p data/
+mkdir -p data/en-pali
+cd data/en-pali
+echo "Cloning raw data source"
+git clone https://github.com/suttacentral/bilara-data
+cd bilara-data/.scripts/bilara-io
+./sheet_export.py vinaya vinaya.tsv --include root,translation+en
+./sheet_export.py sutta sutta.tsv --include root,translation+en
+cp vinaya.tsv ../../../
+cp sutta.tsv ../../../
+cd ../../../../..
 echo "Cleaning and reformatting data..."
 python pali_tsv_to_corpora.py --corpus-root data/en-pali/ data/en-pali/sutta.tsv data/en-pali/vinaya.tsv
